cb7a2b3264f8ae689bc63e071bf9bf242b065b0d
Nadina Siddiqui <nadinasiddiqui@msn.com>
2021-04-29 11:14:04 +0200

    removed PersonType

--- src/Database/(EfCore)/Adult.cs
+++ src/Database/(EfCore)/Adult.cs
@@ -8,9 +8,7 @@ namespace KidsTown.Database
     public partial class Adult
     {
         public int PersonId { get; set; }
-        public int PersonTypeId { get; set; }
         public string PhoneNumber { get; set; }
-        public DateTime UpdateDate { get; set; }
 
         public virtual Person Person { get; set; }
     }
