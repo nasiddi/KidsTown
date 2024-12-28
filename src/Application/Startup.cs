5eedf5d2391fe655265757d732aaebbce1af249e
Nadina.Siddiqui <nadinasiddiqui@msn.com>
2024-12-28 08:35:03 +0100

    fix path

--- src/Application/Startup.cs
+++ src/Application/Startup.cs
@@ -149,7 +149,7 @@ public class Startup(IConfiguration configuration)
         {
             app.UseStaticFiles(new StaticFileOptions
             {
-                FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "ClientApp/kidstown/out")),
+                FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "wwwroot")),
                 RequestPath = ""
             });
         }
